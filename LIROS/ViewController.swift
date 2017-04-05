830718c8d7eae76236ed19d0680ba99f52a19898
FoolOfShadows <foolofshadows@gmail.com>
2017-04-05 08:50:17 -0500

    Initial Commit

---
+++ LIROS/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  LIROS
+//
+//  Created by Fool on 4/5/17.
+//  Copyright © 2017 Fulgent Wake. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: Any? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+
