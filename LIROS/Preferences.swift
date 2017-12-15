f0c249a3285bc31829be429263b0592878a4e6d3
FoolOfShadows <foolofshadows@gmail.com>
2017-12-15 12:21:55 -0600

    Getting ready to branch in order to add a preference pain

---
+++ LIROS/Preferences.swift
@@ -0,0 +1,18 @@
+//
+//  Preferences.swift
+//  LIROS
+//
+//  Created by Fool on 12/15/17.
+//  Copyright © 2017 Fulgent Wake. All rights reserved.
+//
+
+import Cocoa
+
+class Preferences: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
